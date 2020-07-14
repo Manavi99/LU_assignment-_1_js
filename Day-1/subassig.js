eeca351702f4c10b3fef2a83d8646e70e433eb5f
Manavi99 <63236168+Manavi99@users.noreply.github.com>
2020-07-15 01:19:40 +0530

    Add files via upload

---
+++ Day-1/subassig.js
@@ -0,0 +1,7 @@
+console.clear(); //used to clear the console
+var a = {"name" : "bcd", "age":20};
+var b = {"name": "xyz" , "age" :21};
+
+console.log({a ,b }) 
+console.table({a ,b }) //display in table format
+console.trace(a);// to trace the data
